0010598ee2c2e57f9385bd1cd1a401f2ad4946db
ghostrider <ghostrider@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-05 18:48:18 +0000

    adding doc.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqr@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,8 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+
+<p>Please proceed either to <a href="http://www.activequant.org">AQ.org</a> or to the project summary page (link underneath). </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
