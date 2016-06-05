786e86b26f9a274dd73b6433d09720b4d12632a7
ghostrider <ghostrider@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-05 09:35:37 +0000

    adding a d8 method.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqr@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/backtest.R
@@ -0,0 +1,56 @@
+#### backtesting functions ... 
+
+
+
+#' method to generate a pnl curve from a running position. 
+#' bids, asks and running position must have the same length. 
+#' Can compute the pnl from one price to the other, but only for one asset! 
+#' Does not take time into account - if you need signal delays, lag 
+#' all input data on your own.  
+#' 
+#' @param bidPrices an array of bid prices
+#' @param askPrices an array of ask prices
+#' @param runningPosition an array that contains a vector of the position
+#' @param messages specifies whether you want to have debug messages or not, defaults to FALSE
+#' 
+#' @return This function returns a plain double array with pnl changes (uncumulated) and not an XTS series, aligned to your input bid/ask series
+#' 
+#' @note all input arrays must have the same length. 
+generatePnlCurve <- function(bidPrices, askPrices, runningPosition, messages=FALSE)
+{
+        # checks if the length of bid, ask and running position are equally long.
+        if(messages)message("Generating PNL curve.")
+        if(length(bidPrices) == length(askPrices) && length(askPrices) == length(runningPosition))
+        {       
+                .C("c_generatePnlCurve", as.double(bidPrices), as.double(askPrices), as.double(runningPosition), as.integer(length(bidPrices)), pnl = double(length(bidPrices)), PACKAGE="aqr")$pnl
+        }
+        else
+        {
+                # throw an error.
+                simpleError("Arrays must be of same length", "generatePnlCurve")
+        }
+}
+
+#' Applies an approximated StopLoss/TakeProfit strategy on an incoming pnl series. 
+#' 
+#' @return the rewritten pnl series. 
+approximateSLTP <- function(high, low, close, takeProfit, stopLoss, runningPosition, messages=FALSE)
+{
+        # checks if the length of bid, ask and running position are equally long.
+        if(messages){
+      	  message("Generating PNL curve.")
+      	  message(high, "/", low, "/", close, "/", takeProfit, "/" , stopLoss, "/", runningPosition)
+      	}
+      	
+        if(length(high) == length(runningPosition))
+        {       
+            x = .C("c_approximateStopLossTakeProfit", as.double(high), as.double(low), as.double(close), as.double(runningPosition),as.integer(length(runningPosition)),  stopLoss, takeProfit, 
+		            pnl = double(length(high)), position = double(length(high)), PACKAGE="aqr")		
+		        return(cbind(x$pnl, x$position))
+        }
+        else
+        {
+                # throw an error.
+                simpleError("Arrays must have the same length", "approximateSLTP")
+        }
+}
--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -91,75 +91,27 @@ aqDropHours <- function(x, hours){
 
 
 
-#' method to generate a pnl curve from a running position. 
-#' bids, asks and running position must have the same length. 
-#' Can compute the pnl from one price to the other, but only for one asset! 
-#' Does not take time into account - if you need signal delays, lag 
-#' all input data on your own.  
-#' 
-#' @param bidPrices an array of bid prices
-#' @param askPrices an array of ask prices
-#' @param runningPosition an array that contains a vector of the position
-#' @param messages specifies whether you want to have debug messages or not, defaults to FALSE
-#' 
-#' @return This function returns a plain double array with pnl changes (uncumulated) and not an XTS series.
-#' 
-#' @note all input arrays must have the same length. 
-generatePnlCurve <- function(bidPrices, askPrices, runningPosition, messages=FALSE)
-{
-        # checks if the length of bid, ask and running position are equally long.
-        if(messages)message("Generating PNL curve.")
-        if(length(bidPrices) == length(askPrices) && length(askPrices) == length(runningPosition))
-        {       
-                .C("c_generatePnlCurve", as.double(bidPrices), as.double(askPrices), as.double(runningPosition), as.integer(length(bidPrices)), pnl = double(length(bidPrices)), PACKAGE="aqr")$pnl
-        }
-        else
-        {
-                # throw an error.
-                simpleError("Arrays must have the same length", "generatePnlCurve")
-        }
-}
 
-#' Applies an approximated StopLoss/TakeProfit strategy on an incoming pnl series. 
-#' 
-#' @return the rewritten pnl series. 
-approximateSLTP <- function(high, low, close, takeProfit, stopLoss, runningPosition, messages=FALSE)
-{
-        # checks if the length of bid, ask and running position are equally long.
-        if(messages){
-      	  message("Generating PNL curve.")
-      	  message(high, "/", low, "/", close, "/", takeProfit, "/" , stopLoss, "/", runningPosition)
-      	}
-      	
-        if(length(high) == length(runningPosition))
-        {       
-            x = .C("c_approximateStopLossTakeProfit", as.double(high), as.double(low), as.double(close), as.double(runningPosition),as.integer(length(runningPosition)),  stopLoss, takeProfit, 
-		            pnl = double(length(high)), position = double(length(high)), PACKAGE="aqr")		
-		        return(cbind(x$pnl, x$position))
-        }
-        else
-        {
-                # throw an error.
-                simpleError("Arrays must have the same length", "approximateSLTP")
-        }
-}
-
-
-#' returns today as date8. 
+#' returns today
 #' @return a POSIXlt object of now. 
 today <- function(){
   z <- as.POSIXlt(Sys.time())
   return(z)
 }
 
-
-
-#' returns the date one month (30 days) ago as date8
-#' @return a POSIXlt object pointing at thirty days ago
-oneMonthAgo <- function(){
+#' returns the date one month (30 days by default) 
+#' @return a POSIXlt object 
+#' @param d amount of days to shift, default: 30
+daysAgo <- function(d=30){
   z <- as.POSIXlt(Sys.time())
-  z <- as.POSIXlt(z - (3600 * 24 * 30))
+  z <- as.POSIXlt(z - (3600 * 24 * d))
   return(z)
 }
 
+#' Formats an input POSIX date to Date8 format (YYYYMMDD). This function is a convenience function. 
+#' @param dateObj a POSIX date object
+#' @return an integer YYYYMMDD representation 
+d8 <- function(dateObj) {
+	as.integer(strftime(dateObj, format="%Y%d%m"))
+}
 
